9d4f768d1501a58ae5c0c831481e9010d2ae7038
Rajat Rawat <rajatrawatprofess@gmail.com>
2021-01-26 13:31:01 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! rambo is great'));
+app.get('/', (req, res) => res.send('Hello World! Disco dancer'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
